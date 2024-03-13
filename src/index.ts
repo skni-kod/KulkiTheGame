d1543a044d83bc090bfb561df442d81671cb2f4f
Skalik <106670239+Skalik2@users.noreply.github.com>
2024-03-13 22:10:44 +0100

    add: local storage management

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import { LocalStorage } from "./localStorage.js";
 import { Test } from "./test.js";
 
 const el = document.getElementById("game");
 console.log(el);
 
 const test = new Test();
+
+
+LocalStorage.init();
+LocalStorage.removeItem("b10");
+console.log(LocalStorage.consoleLog());
