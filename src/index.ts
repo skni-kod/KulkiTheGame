5ba74fa77debcb3059d678ae8ca07a4e4ec99053
Skalik <106670239+Skalik2@users.noreply.github.com>
2024-03-16 19:28:24 +0100

    Refactor: everything++

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ if (context === null) {
     throw new Error('2D content not available');
 }
 
+// do wyjebnia do innego pliku
 let ballsList = {
     b1: {
         x: canvas.width/2,
@@ -49,37 +50,134 @@ let ballsList = {
     },
 }
 
-let moveBall = function (object) {
-    if(object.x + object.r/2 >= canvas.width || object.x - object.r/2 < 0) {
-        object.dX *= -1;
-
-    }
-    if(object.y - object.r/2 < 0 || object.y + object.r/2 >= canvas.height ){
-        object.dY *= -1;
-    }
-    object.x += object.dX * object.speed;
-    object.y += object.dY * object.speed;
-}
-
-let drawBall = function (object) {
-    context.beginPath();
-    context.arc(object.x, object.y, object.r, Math.random(), Math.PI * 2);
-    context.fillStyle = object.color;
-    context.fill();
-    moveBall(object);
-}
-
-let ball1 = Object.create(ballsList.b1);
-let ball2 = Object.create(ballsList.b3);
 LocalStorage.init();
-LocalStorage.removeItem("b10");
-console.log(LocalStorage.consoleLog());
+let stage: number = LocalStorage.getItem("level");
+let money: number  = LocalStorage.getItem("money");
+let mouseX: number = 0; 
+let mouseY: number = 0;
+let loopInterval = setInterval(loop,10); 
+let bricks = new Array();
+let balls = new Array();
 
-while (true) {
-    const sleep = (ms) => new Promise(r => setTimeout(r, ms));
-    await sleep(10);
-    context.clearRect(0, 0, 1600, 1000);
-    context.fillStyle = "white";
-    drawBall(ball1);
-    drawBall(ball2);
+initGame();
+
+console.log(bricks);
+
+function initGame(){
+    drawBoard(34);
+    createBall(ballsList.b1);
+    createBall(ballsList.b2);
+    createBall(ballsList.b3);
+    createBall(ballsList.b4);
+}
+
+function drawBoard(numberOfBricks: number){
+    for (let i: number = 0; i < numberOfBricks; i++){
+        drawBrick();
+    }
+}
+
+function clearContext(){
+    context.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function loop(){
+    draw();
+}
+
+function draw(){
+    clearContext();
+    updateBricks();
+    updateBalls();
+}
+
+//Bricks
+function Brick(x:number, y:number, width:number, height:number){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = "#25f522";
+    this.strokeStyle = "black";
+
+    this.update = () => {
+        context.fillStyle = this.color;
+        context.fillRect(this.x,this.y,this.width,this.height);
+        context.strokeStyle = this.strokeStyle;
+        context.strokeRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function drawBrick(){
+    let offsetX: number = canvas.width / 16;
+    let offsetY: number = canvas.height / 32;
+    let x = 0;
+    let y = 0;
+
+    if (bricks.length!=0){
+        x = bricks[bricks.length - 1].x;
+        y = bricks[bricks.length - 1].y;
+
+        if (bricks[bricks.length - 1].x > canvas.width - offsetX){
+            y += offsetY;
+            x = 0;
+        }else{
+            x += offsetX;
+        }
+    }
+    createBrick(x,y,offsetX,offsetY);
+}
+
+function createBrick(x: number, y:number, width: number, height: number){
+    let brick = new Brick(x,y,width,height);
+    bricks.push(brick);
+}
+
+function updateBricks(){
+    for(let i:number = 0; i < bricks.length; i++){
+        bricks[i].update();
+    }
+}
+
+//Balls ༼ つ ◕_◕ ༽つ
+function createBall(Object){
+    let ball = new Ball(Object);
+    balls.push(ball);
+}
+
+function updateBalls(){
+    for(let i:number = 0; i < balls.length; i++){
+        balls[i].update();
+    }
+}
+
+function Ball(Object){
+    this.x = Object.x;
+    this.y = Object.y;
+    this.dX = Math.random();
+    this.dY = 1 - this.dX;
+    this.speed = 4;
+    this.radius = Object.r;
+    this.color = Object.color;
+
+    this.update = () =>{
+        this.y+=this.dY;
+		this.x+=this.dX;
+		this.bounds();
+		context.fillStyle = this.color;
+		context.beginPath();
+		context.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
+		context.fill();
+    }
+
+    this.bounds = function(){
+		if(this.x + this.radius/2 >= canvas.width || this.x - this.radius/2 < 0) {
+            this.dX *= -1;
+        }
+        if(this.y - this.radius/2 < 0 || this.y + this.radius/2 >= canvas.height ){
+            this.dY *= -1;
+        }
+        this.x += this.dX * this.speed;
+        this.y += this.dY * this.speed;
+	}
 }
