1ff234620b7feb344d58839546ff76963361de9c
karolmichonsky <karolmichonsky@gmail.com>
2024-03-16 15:20:22 +0100

    Refactor: Map for Ball objs, update draw
    -Refactor balls object so it proivdes information for all balls
    - Change draw function to match new object drawing
    - Generate object

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,42 @@ if (context === null) {
     throw new Error('2D content not available');
 }
 
-let ballObject = {
-    x: 10,
-    y: 10,
-    r: 15,
-    color: "red"
+let ballsList = {
+    b1: {
+        x: canvas.width/2,
+        y: canvas.height/2,
+        color: "yellow",
+        function: "basic"
+    },
+    b2: {
+        x: canvas.width/2,
+        y: canvas.height/2,
+        color: "green",
+        function: "touch"
+    },
+    b3: {
+        x: canvas.width/2,
+        y: canvas.height/2,
+        color: "blue",
+        function: "sniper"
+    },
+    b4: {
+        x: canvas.width/2,
+        y: canvas.height/2,
+        color: "purple",
+        function: "explode"
+    },
 }
 
-let drawBall = function () {
+
+let drawBall = function (object) {
     context.beginPath();
-    context.arc(ballObject.x, ballObject.y, ballObject.r, 0, Math.PI * 2);
-    context.fillStyle = ballObject.color;
+    context.arc(object.x, object.y, 15, 0, Math.PI * 2);
+    context.fillStyle = object.color;
     context.fill();
-
 }
 
-context.clearRect(0, 0, 1600, 1000);
-drawBall();
-
+let ball1 = Object.create(ballsList.b2);
 LocalStorage.init();
 LocalStorage.removeItem("b10");
 console.log(LocalStorage.consoleLog());
@@ -31,8 +49,7 @@ console.log(LocalStorage.consoleLog());
 while (true) {
     const sleep = (ms) => new Promise(r => setTimeout(r, ms));
     await sleep(50);
-    ballObject.x += 1;
-    context.fillStyle = "white";
     context.clearRect(0, 0, 1600, 1000);
-    drawBall();
+    context.fillStyle = "white";
+    drawBall(ball1);
 }
