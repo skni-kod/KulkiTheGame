f50e6fdfbcc3a158a6f49f881f38500e2f5303fc
Szymon Jabłoński <szyfzjablonski@gmail.com>
2024-03-26 13:58:23 +0100

    Refactor: brick collision
    - started implementing the new algorithm
    - experimental as of now
    - need to add another table or more indexes to allow for independent brick and line offsets for ease of calculation
    - gotta walk my dogs for now, will do more later

--- src/balls.ts
+++ src/balls.ts
@@ -22,7 +22,7 @@ export let ballsList = {
         color: "blue",
         brickFunction: "basic",
         wallFunction: "sniper",
-        speed: 500,
+        speed: 2,
     },
     b4: {
         r: 10,
@@ -88,6 +88,94 @@ export function Ball(Object){
             // the ratio is the absolute ratio of distance to the wall to the vector in given axis, 0 means no collision
             let ratioY : number = 0;
             let ratioX : number = 0;
+
+            {
+                // calculating brick collisions, attempt No 2
+                // a lot of these calculations will be moved to the beginning and saved in the object to improve speed
+
+                // first define a distance to travel between bound checks
+                // too small will lead to decreased performance, too high may result in clipping
+                // it will need to be optimized via experiments
+                const travelDistance : number = 1;
+
+                // calculate the travel in x and y to know how much to move without much calculation
+                // this should be faster too, oh wait, this is javascript
+                const travelX : number = this.dX / this.speed * travelDistance;
+                const travelY : number = this.dY / this.speed * travelDistance;
+
+                // creating a brick offset array, will hold data about which bricks to check collision with
+                let brickTable = [[], [], []];
+                // x, y, corner order
+                if (travelX > 0) {
+                    brickTable[0] = [1, 0];
+                    if (travelY > 0) {
+                        brickTable[1] = [0, 1];
+                        brickTable[2] = [1, 1];
+                    }
+                    else {
+                        brickTable[1] = [0, 0];
+                        brickTable[2] = [1, 0];
+                    }
+                }
+                else {
+                    brickTable[0] = [0, 0];
+                    if (travelY > 0) {
+                        brickTable[1] = [0, 1];
+                        brickTable[2] = [0, 1];
+                    }
+                    else {
+                        brickTable[1] = [0, 0];
+                        brickTable[2] = [0, 0];
+                    }
+                }
+
+                // now start a loop, going this distance and stopping when it's reached, probably a break will exit it
+                let currentX = this.x;
+                let currentY = this.y;
+                while (currentX != afterX && currentY != afterY) {
+
+                    // calculate the coords to check from
+                    let coordX = Math.floor(currentX / brickX);
+                    let coordY = Math.floor(currentY / brickY);
+
+                    // move the ball to the new position
+                    if (Math.abs(currentX - afterX) < Math.abs(travelX) && Math.abs(currentY - afterY) < Math.abs(travelY)) {
+                        currentX = afterX;
+                        currentY = afterY;
+                    }
+                    else {
+                        currentX += travelX;
+                        currentY += travelY;
+                    }
+
+
+
+                    // checking collision with the bricks from the table
+
+
+                    // first x, check if the brick can actually be checked and if it still exists
+
+                    if (coordX > 0 && coordX < 15 && bricks[getIndex(coordX + brickTable[0][0], coordY)].hp > 0) {
+                        // now check if it collides
+                        //console.log("     ", Math.abs((coordX + brickTable[0][0]) * brickX - currentX), this.radius);
+                        if (Math.abs((coordX + brickTable[0][0]) * brickX - currentX) <= this.radius) {
+                            console.log("Collision on X!");
+                            this.dX *= -1;
+                            return;
+                        }
+                    }
+
+                    if (coordX > 0 && coordX < 15 && coordY > 0 && coordY < 31 && bricks[getIndex(coordX + brickTable[2][0], coordY + brickTable[2][1])].hp > 0) {
+                        // now check if it collides
+                        //console.log("     ", Math.abs((coordX + brickTable[2][0]) * brickX - currentX), this.radius);
+                        if (Math.abs((coordX + brickTable[2][0]) * brickX - currentX) <= this.radius) {
+                            console.log("Collision on X!");
+                            this.dX *= -1;
+                            return;
+                        }
+                    }
+                }
+            }
 
 
 
